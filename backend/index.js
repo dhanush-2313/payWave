15242dbd4e2a44b97ababf5a8770082331f586a5
dhanush-2313 <dhanusu006@gmail.com>
2024-08-16 00:24:47 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const rootRoute = require("./routes/index");
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +12,4 @@ app.use(function (req, res, err, next) {
   res.json({ msg: "some error occured" });
   next();
 });
-app.listen(3000);
+app.listen(port);
